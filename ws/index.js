e77a85c6e411e6af5c9f8ca7f892b119952aa982
Pavel Lang <langpavel@phpskelet.org>
2017-11-11 13:40:12 +0100

    Basic WS protocol

--- ws/index.js
+++ ws/index.js
@@ -1,9 +1,12 @@
 const WebSocket = require('faye-websocket');
-const pg = require('pg');
+// const pg = require('pg');
 
 function log(msg, ...rest) {
   console.info(`${new Date().toISOString()} ${msg}`, ...rest);
 }
+
+// parse incomming message in form: [clientId] command
+const reCmd = /^(?:\[([^\]]+)\])?\s*(\S+)/;
 
 const upgradeHandler = (req, socket, head) => {
   if (WebSocket.isWebSocket(req)) {
@@ -12,14 +15,44 @@ const upgradeHandler = (req, socket, head) => {
     let wsState = Object.create(null);
     wsState.lastMessage = null;
 
+    ws.json = (json) => ws.send(JSON.stringify(json));
+
     ws.on('message', (event) => {
-      log('Received', event.data);
-      if (event.data === 'repeat') {
-        ws.send(wsState.lastMessage);
-      } else {
-        wsState.lastMessage = event.data;
-        ws.send(event.data);
+      const raw = event.data;
+      log('Received', raw);
+
+      // accept only strings
+      if (typeof raw !== 'string') {
+        return ws.json({
+          error: 'Invalid input',
+        });
       }
+
+      // return empty JSON on empty input
+      if (!raw.length) {
+        return ws.json({});
+      }
+      let message = null;
+
+      // accept JSON input
+      if (raw[0] === '{') {
+        try {
+          message = JSON.parse(raw);
+        } catch(error) {
+          return ws.json({
+            error: 'Invalid JSON',
+          });
+        }
+      } else {
+        const [, cid, cmd] = reCmd.exec(raw);
+        message = {
+          cid,
+          cmd,
+          text: cid ? raw.slice(cid.length + 2) : raw, // strip initial '[clientId]'
+        }
+      }
+
+      log(`[${message.cid}]`, message);
     });
 
     ws.on('close', (event) => {
